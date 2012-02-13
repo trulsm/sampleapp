a9f3343e99c2a914bd4b1db044d9941a4b24d359
Truls Michalsen <truls.michalsen@gmail.com>
2012-02-13 21:18:55 +0100

    User model modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
-class User < ActiveRecord::Base
-  attr_accessible :name, :email
-end
-
 # == Schema Information
 #
 # Table name: users
@@ -12,4 +8,17 @@ end
 #  created_at :datetime
 #  updated_at :datetime
 #
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,  :presence   => true,
+                    :length     => { :maximum => 50 }
+  validates :email, :presence   => true, 
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+end
+
 
