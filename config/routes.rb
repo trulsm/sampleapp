a6426340d87c159a011c5149fc4c90cfdc00338e
Truls Michalsen <truls.michalsen@gmail.com>
2012-02-13 21:49:16 +0100

    Added a stub User show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Sampleapp::Application.routes.draw do
-
+  
+  resource :users
 
   root :to => "pages#home"
-  match '/about', :to => 'pages#about'
-  match '/contact', :to => 'pages#contact'
-  match '/help', :to => 'pages#help'
   
-  match '/signup', :to => 'users#new'
+  match '/about',   :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
 
 
 
