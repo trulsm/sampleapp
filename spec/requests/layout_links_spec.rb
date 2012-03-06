2f4a6375dbad4cbfe47d94c573f594c2663cd1ef
Truls Michalsen <truls.michalsen@gmail.com>
2012-03-06 21:15:05 +0100

    Working index page

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -70,23 +70,25 @@ describe "LayoutLinks" do
     it "should have a sign out link" do
       visit root_path
       response.should have_selector("a", :href => signout_path,
-                                         :content => "Sign out"
-      )
+                                         :content => "Sign out")
     end
     
     it "should have a profile link" do
       visit root_path
       response.should have_selector("a", :href => user_path(@user),
-                                         :content => "Profile"
-      )
+                                         :content => "Profile")
     end
-        it "should have a settings link" do
+    
+    it "should have a settings link" do
       visit root_path
       response.should have_selector("a", :href => edit_user_path(@user),
-                                         :content => "Settings"
-      )
+                                         :content => "Settings")
     end
     
-    
+    it "should have a users link" do
+      visit root_path
+      response.should have_selector("a", :href => users_path,
+                                         :content => "Users")
+    end
   end
 end
