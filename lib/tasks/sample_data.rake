b13cadc6ee09bf32bdb0bcd5a0ad87b21aa60d17
Truls Michalsen <truls.michalsen@gmail.com>
2012-03-11 20:45:52 +0100

    Can now destroy users (as admins)

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -4,10 +4,11 @@ namespace :db do
   desc "Fill database with sample data"
   task :populate => :environment do
     Rake::Task['db:reset'].invoke
-    User.create!(:name => "Foo Bar",
-                 :email => "foo@bar.com",
-                 :password => "foobar",
-                 :password_confirmation => "foobar")
+    admin = User.create!(:name => "Foo Bar",
+                         :email => "foo@bar.com",
+                         :password => "foobar",
+                         :password_confirmation => "foobar")
+    admin.toggle!(:admin)
     99.times do |n|
       name = Faker::Name.name
       email = "example-#{n+1}@example.com"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -149,7 +149,30 @@ describe User do
       end
     end
   end
+  
+  describe "admin attributes" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+    
+    
+  end
 end
+
 
 
 
@@ -164,5 +187,6 @@ end
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
