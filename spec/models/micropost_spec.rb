f590fca9664b9854cb4bdd9fc8568aebc66b5b52
Truls Michalsen <truls.michalsen@gmail.com>
2012-03-13 20:36:30 +0100

    Added microposts validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Micropost do
@@ -25,19 +36,22 @@ describe Micropost do
       @micropost.user_id.should == @user.id
       @micropost.user.should == @user
     end
-    
   end
   
+  describe "validations" do
+    it "should have a user id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+    
+    it "should require non-blank content" do
+      @user.microposts.build(:content => "   ").should_not be_valid
+    end
+    
+    it "should reject long content" do
+      @user.microposts.build(:content => "a" * 141).should_not be_valid
+    end
+  end
 end
 
-# == Schema Information
-#
-# Table name: microposts
-#
-#  id         :integer         not null, primary key
-#  content    :string(255)
-#  user_id    :integer
-#  created_at :datetime
-#  updated_at :datetime
-#
+
 
